51e980c9cd80e34cc644ecd1b3792ee92c6130da
MelnykovSV <svmelnykov@gmail.com>
2023-03-06 22:51:02 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import { Profile } from './Profile';
 export const App = () => {
   return (
     <div
@@ -7,10 +8,36 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
+      <div>First div</div>
+      <div>Second div</div>
+      <div>Third div</div>
+      <div>Fourth div (Child gonna be below this)</div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <div>Fifth div (Child gonna be above this)</div>
     </div>
   );
+};
+
+const user = {
+  username: 'Jacques Gluke',
+  tag: 'jgluke',
+  location: 'Ocho Rios, Jamaica',
+  avatar: 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { Profile } from 'components/Profile';
+
+const user = {
+  username: 'Jacques Gluke',
+  tag: 'jgluke',
+  location: 'Ocho Rios, Jamaica',
+  avatar: 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+
+const users = [
+  {
+    username: 'Jacques Gluke',
+    tag: 'jgluke',
+    location: 'Ocho Rios, Jamaica',
+    avatar: 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
+    stats: {
+      followers: 5603,
+      views: 4827,
+      likes: 1308,
+    },
+  },
+  {
+    username: 'Petra Marica',
+    tag: '@pmarica',
+    location: 'Salvador, Brasil',
+    avatar: 'https://cdn-icons-png.flaticon.com/512/1077/1077012.png',
+    stats: {
+      followers: 1000,
+      views: 2000,
+      likes: 3000,
+    },
+  },
+];
+
+console.log(user);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
