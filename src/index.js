1305633e5f8c290e2998b1fcab538b0e01233274
MelnykovSV <svmelnykov@gmail.com>
2023-03-15 01:49:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { App } from './components/App';
 import './index.css';
-import { Profile } from 'components/Profile';
+import { Profile } from './components/Profile';
 import user from './data/user.json';
 
 console.log(user);
